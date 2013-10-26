837b8082281f1592e8ca8dfcb36ff13762337b5c
MicrocontrollersAndMore <cdahms123@gmail.com>
2013-10-26 13:33:56 -0400

    commit

---
+++ OCR_0_8/frmOCR.Designer.vb
@@ -0,0 +1,134 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmOCR
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+		Me.lblFile = New System.Windows.Forms.Label()
+		Me.tlpTextBoxAndImageBox = New System.Windows.Forms.TableLayoutPanel()
+		Me.ibImage = New Emgu.CV.UI.ImageBox()
+		Me.txtOCR = New System.Windows.Forms.TextBox()
+		Me.txtFile = New System.Windows.Forms.TextBox()
+		Me.btnFile = New System.Windows.Forms.Button()
+		Me.ofdFile = New System.Windows.Forms.OpenFileDialog()
+		Me.tlpTextBoxAndImageBox.SuspendLayout
+		CType(Me.ibImage,System.ComponentModel.ISupportInitialize).BeginInit
+		Me.SuspendLayout
+		'
+		'lblFile
+		'
+		Me.lblFile.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+		Me.lblFile.Location = New System.Drawing.Point(0, 2)
+		Me.lblFile.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+		Me.lblFile.Name = "lblFile"
+		Me.lblFile.Size = New System.Drawing.Size(96, 20)
+		Me.lblFile.TabIndex = 0
+		Me.lblFile.Text = "choose file:"
+		Me.lblFile.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+		'
+		'tlpTextBoxAndImageBox
+		'
+		Me.tlpTextBoxAndImageBox.ColumnCount = 1
+		Me.tlpTextBoxAndImageBox.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100!))
+		Me.tlpTextBoxAndImageBox.Controls.Add(Me.ibImage, 0, 1)
+		Me.tlpTextBoxAndImageBox.Controls.Add(Me.txtOCR, 0, 0)
+		Me.tlpTextBoxAndImageBox.Location = New System.Drawing.Point(3, 23)
+		Me.tlpTextBoxAndImageBox.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+		Me.tlpTextBoxAndImageBox.Name = "tlpTextBoxAndImageBox"
+		Me.tlpTextBoxAndImageBox.RowCount = 2
+		Me.tlpTextBoxAndImageBox.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 35!))
+		Me.tlpTextBoxAndImageBox.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 65!))
+		Me.tlpTextBoxAndImageBox.Size = New System.Drawing.Size(540, 410)
+		Me.tlpTextBoxAndImageBox.TabIndex = 4
+		'
+		'ibImage
+		'
+		Me.ibImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.ibImage.Dock = System.Windows.Forms.DockStyle.Fill
+		Me.ibImage.Enabled = false
+		Me.ibImage.Location = New System.Drawing.Point(2, 145)
+		Me.ibImage.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+		Me.ibImage.Name = "ibImage"
+		Me.ibImage.Size = New System.Drawing.Size(536, 263)
+		Me.ibImage.TabIndex = 2
+		Me.ibImage.TabStop = false
+		'
+		'txtOCR
+		'
+		Me.txtOCR.Dock = System.Windows.Forms.DockStyle.Fill
+		Me.txtOCR.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+		Me.txtOCR.Location = New System.Drawing.Point(2, 2)
+		Me.txtOCR.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+		Me.txtOCR.Multiline = true
+		Me.txtOCR.Name = "txtOCR"
+		Me.txtOCR.ReadOnly = true
+		Me.txtOCR.ScrollBars = System.Windows.Forms.ScrollBars.Both
+		Me.txtOCR.Size = New System.Drawing.Size(536, 139)
+		Me.txtOCR.TabIndex = 3
+		'
+		'txtFile
+		'
+		Me.txtFile.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+		Me.txtFile.Location = New System.Drawing.Point(98, 0)
+		Me.txtFile.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+		Me.txtFile.Name = "txtFile"
+		Me.txtFile.Size = New System.Drawing.Size(419, 25)
+		Me.txtFile.TabIndex = 5
+		'
+		'btnFile
+		'
+		Me.btnFile.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+		Me.btnFile.Location = New System.Drawing.Point(518, 2)
+		Me.btnFile.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+		Me.btnFile.Name = "btnFile"
+		Me.btnFile.Size = New System.Drawing.Size(24, 20)
+		Me.btnFile.TabIndex = 6
+		Me.btnFile.Text = "..."
+		Me.btnFile.UseVisualStyleBackColor = true
+		'
+		'frmOCR
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(546, 435)
+		Me.Controls.Add(Me.btnFile)
+		Me.Controls.Add(Me.txtFile)
+		Me.Controls.Add(Me.tlpTextBoxAndImageBox)
+		Me.Controls.Add(Me.lblFile)
+		Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+		Me.Name = "frmOCR"
+		Me.Text = "OCR Example"
+		Me.tlpTextBoxAndImageBox.ResumeLayout(false)
+		Me.tlpTextBoxAndImageBox.PerformLayout
+		CType(Me.ibImage,System.ComponentModel.ISupportInitialize).EndInit
+		Me.ResumeLayout(false)
+		Me.PerformLayout
+
+End Sub
+    Friend WithEvents lblFile As System.Windows.Forms.Label
+    Friend WithEvents tlpTextBoxAndImageBox As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents ibImage As Emgu.CV.UI.ImageBox
+    Friend WithEvents txtOCR As System.Windows.Forms.TextBox
+    Friend WithEvents txtFile As System.Windows.Forms.TextBox
+    Friend WithEvents btnFile As System.Windows.Forms.Button
+    Friend WithEvents ofdFile As System.Windows.Forms.OpenFileDialog
+
+End Class
---
+++ OCR_0_8/frmOCR.vb
@@ -0,0 +1,103 @@
+﻿Option Strict
+
+Imports Emgu.CV
+Imports Emgu.Util
+Imports Emgu.CV.OCR
+Imports Emgu.CV.Structure
+Imports Emgu.CV.UI
+Imports Emgu.CV.CvEnum
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Public Class frmOCR
+
+' member variables ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Dim blnFirstTimeInResizeEvent As Boolean = True
+Dim intOrigFormWidth As Integer
+Dim intOrigFormHeight As Integer
+Dim intOrigTableLayoutPanelWidth As Integer
+Dim intOrigTableLayoutPanelHeight As Integer
+
+Dim imgImage As Image(Of Bgr, Byte) = Nothing
+Dim imgBlank As Image(Of Bgr, Byte) = Nothing
+
+Dim tess As Tesseract
+
+' constructor '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Sub New()
+	InitializeComponent()						' This call is required by the designer.
+	
+	intOrigFormWidth = Me.Width
+	intOrigFormHeight = Me.Height
+	intOrigTableLayoutPanelWidth = tlpTextBoxAndImageBox.Width
+	intOrigTableLayoutPanelHeight = tlpTextBoxAndImageBox.Height
+
+	Try
+		tess = New Tesseract("tessdata", "eng", Tesseract.OcrEngineMode.OEM_DEFAULT)
+	Catch ex As Exception
+		Me.Text = "error instantiating Tesseract"
+		txtOCR.Text = "error instantiating Tesseract"
+		txtFile.Enabled = False
+		btnFile.Enabled = False
+		Return
+	End Try
+End Sub
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Private Sub frmOCR_Resize( sender As System.Object,  e As System.EventArgs) Handles MyBase.Resize
+	'This If Else statement is necessary to throw out the first time the Form1_Resize event is called.
+	'For some reason, in VB.NET the Resize event is called once before the constructor, then the constructor is called,
+	'then the Resize event is called each time the form is resized.  The first time the Resize event is called
+	'(i.e. before the constructor is called) the coordinates of the components on the form all read zero,
+	'therefore we have to throw out this first call, then the constructor will run and get the correct initial
+	'component location data, then every time after that we can let the Resize event run as expected
+	If(blnFirstTimeInResizeEvent = True) Then
+		blnFirstTimeInResizeEvent = False
+	Else
+		tlpTextBoxAndImageBox.Width = Me.Width - (intOrigFormWidth - intOrigTableLayoutPanelWidth)
+		tlpTextBoxAndImageBox.Height = Me.Height - (intOrigFormHeight - intOrigTableLayoutPanelHeight)
+	End If
+End Sub
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Private Sub btnFile_Click( sender As System.Object,  e As System.EventArgs) Handles btnFile.Click
+	Dim dialogResult As DialogResult = ofdFile.ShowDialog()
+
+	If(dialogResult = Windows.Forms.DialogResult.OK Or dialogResult = Windows.Forms.DialogResult.Yes) Then
+		txtFile.Text = ofdFile.FileName
+	End If
+End Sub
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Private Sub txtFile_TextChanged( sender As System.Object,  e As System.EventArgs) Handles txtFile.TextChanged
+	txtFile.SelectionStart = txtFile.Text.Length								'move caret to end of text box so file name is visible
+	If(txtFile.Text <> String.Empty) Then
+		ProcessImageAndUpdateGUI()
+	End If
+End Sub
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Sub ProcessImageAndUpdateGUI()	
+	Try
+		imgImage = New Image(Of Bgr, Byte)(txtFile.Text)
+	Catch ex As Exception
+		Me.Text = "error opening file"
+		Return
+	End Try
+	
+	If(imgImage Is Nothing) Then
+		Return
+	End If
+	
+	Me.Text = "processing, please wait . . ."
+	ibImage.Image = imgBlank
+	Application.DoEvents()
+	
+	tess.Recognize(imgImage)
+	txtOCR.Text = tess.GetText()
+	
+	ibImage.Image = imgImage
+	
+	Me.Text = "done processing, choose another image if desired"
+End Sub
+
+End Class
